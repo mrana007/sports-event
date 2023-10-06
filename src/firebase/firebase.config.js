8b35df4ad42dfa90c891f5ba5aea3ffae7aa4772
mrana007 <rana.es.bc@gmail.com>
2023-10-06 22:00:45 +0600

    initialized basic project setup with reat router, firebase, tailwind css and daisyui

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwG4In0cqHfqE8jVSdBsqeDEKSwUMKS4s",
+  authDomain: "sports-event-4243b.firebaseapp.com",
+  projectId: "sports-event-4243b",
+  storageBucket: "sports-event-4243b.appspot.com",
+  messagingSenderId: "768074654926",
+  appId: "1:768074654926:web:c460a57f7ad200953c9c9f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
